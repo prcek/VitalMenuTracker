344b2402d1dd7a0b07ffc1801fa020113375ad64
Tomas <tomas.hluchan@gmail.com>
2011-06-27 14:13:23 +0200

    deferred reporty

--- reports/views.py
+++ reports/views.py
@@ -9,6 +9,7 @@ from utils.decorators import user_required, power_required, admin_required, cron
 from utils.mail import send_mail_to_admins
 from accounts.utils import getAccountsReport, getDetailAccountReport
 from accounts.models import Account
+from google.appengine.ext import deferred
 
 
 import logging
@@ -17,6 +18,31 @@ import logging
 def index(request):
     return render_to_response('reports/index.html', RequestContext(request))
 
+def deferred_one_trans_report(account_id=None, account_email=None):
+    logging.info('do_one_trans_report(%d,"%s")' % (account_id,account_email))
+    account_report = 'detail ' + getDetailAccountReport(account_id)
+    logging.info('account report: "%s"' % account_report)
+    send_mail_to_admins('VMTracker account id:%d report' % account_id, account_report)
+
+def deferred_accounts_report():
+    account_report = getAccountsReport()
+    logging.info('accounts report: %s', account_report)
+    send_mail_to_admins('VMTracker daily report',account_report)
+ 
+   
+def do_trans_reports():
+    account_list = Account.objects.all().fetch(100)
+    for account in account_list:
+        account_id = account.key().id()
+        account_email = '' # account.owner_email
+        logging.info('add task for "do_one_trans_report(%d,"%s")"' % (account_id, account_email))
+        deferred.defer(deferred_one_trans_report, account_id, account_email)
+    logging.info('all tasks planned')
+
+def do_accounts_report():
+    deferred.defer(deferred_accounts_report)
+        
+
 @cron_required
 def cron_test(request):
     logging.info('cron test')
@@ -24,6 +50,7 @@ def cron_test(request):
 #    account_id = Account.objects.all().get().key().id()
 #    account_report = 'detail ' + getDetailAccountReport(account_id)
 #    logging.info('account report: "%s"' % account_report)
+#    do_trans_reports()
     if request.cron_request:
         logging.info('HttpResponse ok')
         return HttpResponse('ok')
@@ -32,9 +59,8 @@ def cron_test(request):
 @cron_required
 def cron_daily(request):
     logging.info('cron daily')
-    account_report = getAccountsReport()
-    logging.info('accounts report: %s', account_report)
-    send_mail_to_admins('VMTracker daily report',account_report)
+    do_accounts_report()
+    do_trans_reports()
     if request.cron_request:
         logging.info('HttpResponse ok')
         return HttpResponse('ok')
