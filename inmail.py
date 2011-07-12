2d0e0ff77455f0a1d69156e42b47e0a9f781a2b9
Tomas <tomas.hluchan@gmail.com>
2011-07-12 16:37:20 +0200

    test priloh

--- inmail.py
+++ inmail.py
@@ -6,6 +6,11 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
+#        if mail_message.attachments:
+#            pass
+#        else:
+#            for a in mail_message.attachments:
+#                logging.info('attachment name %s'% a[0])
 
 
 application = webapp.WSGIApplication([LogSenderHandler.mapping()], debug=True)
