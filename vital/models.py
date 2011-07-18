03ee88c43da4dc5803525506cba6994e17b932d0
Tomas <tomas.hluchan@gmail.com>
2011-07-18 08:34:50 +0200

    orderitem v zakladani transakce v listboxy je citelnejsi

--- vital/models.py
+++ vital/models.py
@@ -44,4 +44,10 @@ class OrderItem(BaseModel):
             self.undo_request = False 
          
         return True
+    def __unicode__(self):
+        if self.key():
+            return '[%s] %s %s %d' % (self.key().id(),self.date,self.name,self.cost)
+        else:
+            return '%s %s %d' % (self.date,self.name,self.cost)
+
       
