dbe6fbcdbb12c76228a6207e782ba7f0799b7218
Tomas <tomas.hluchan@gmail.com>
2011-07-22 14:53:09 +0200

    default config

--- utils/config.py
+++ utils/config.py
@@ -16,12 +16,12 @@ def createConfig(name,value):
         c.save()
 
 def setupConfig():
-    createConfig('REPORT_DAILY_SUMMARY','1')    
-    createConfig('REPORT_DAILY_TRANSACTIONS','1')
+    createConfig('REPORT_DAILY_SUMMARY','0')    
+    createConfig('REPORT_DAILY_TRANSACTIONS','0')
     createConfig('PDF_TEST_TEXT',u'Příliš žluťoučký kůň úpěl ďábelské ódy (.CZ?!)')
-    createConfig('CLEARANCE_ALL_ORDER_ITEMS','1')
+    createConfig('CLEARANCE_ALL_ORDER_ITEMS','0')
     createConfig('ADMIN_EMAIL','admin@domain.com') 
     createConfig('MAIL_TEST_TO','user@domain.com') 
     createConfig('MAIL_TEST_FROM','admin@domain.com') 
-    createConfig('MAIL_TEST','1') 
+    createConfig('MAIL_TEST','0') 
     
